02f296bd271b60fc2bcfe682c46217a140a7bcaf
nexsus57 <loginerw@gmail.com>
2025-12-30 14:49:39 +0530

    Update page.tsx

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,6 +1,8 @@
+
 'use client';
 
-import AdminLoginPage from '../../../pages/admin/AdminLoginPage';
+import React from 'react';
+import AdminLoginPage from '@/pages/admin/AdminLoginPage';
 
 export default function Login() {
   return <AdminLoginPage />;
