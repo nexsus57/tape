778c4165ca01a8f60bde427cadf4b583f1bf38b9
nexsus57 <loginerw@gmail.com>
2026-05-17 23:22:39 +0530

    Create build-sitemap.ts

---
+++ build-sitemap.ts
@@ -0,0 +1,28 @@
+import fs from 'fs';
+import { PRODUCTS, INDUSTRIES } from './src/constants';
+import { TECHNICAL_BLOGS } from './src/data/blogData';
+
+const BASE_URL = 'https://tapeindia.shop';
+
+const sitemap = [
+    '<?xml version="1.0" encoding="UTF-8"?>',
+    '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+    '  <!-- Main Pages -->',
+    `  <url><loc>${BASE_URL}/</loc><priority>1.0</priority><changefreq>weekly</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/about</loc><priority>0.8</priority><changefreq>monthly</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/products</loc><priority>0.9</priority><changefreq>daily</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/industries</loc><priority>0.9</priority><changefreq>monthly</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/blog</loc><priority>0.8</priority><changefreq>weekly</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/contact</loc><priority>0.8</priority><changefreq>monthly</changefreq></url>`,
+    `  <url><loc>${BASE_URL}/request-quote</loc><priority>0.9</priority><changefreq>monthly</changefreq></url>`,
+    '  <!-- Blog Posts -->',
+    ...TECHNICAL_BLOGS.map(blog => `  <url><loc>${BASE_URL}/blog/${blog.id}</loc><priority>0.7</priority><changefreq>monthly</changefreq></url>`),
+    '  <!-- Products -->',
+    ...PRODUCTS.map(product => `  <url><loc>${BASE_URL}/product/${product.id.toLowerCase().replace(/[^a-z0-9-]+/g, '')}</loc><priority>0.8</priority></url>`),
+    '  <!-- Industries -->',
+    ...INDUSTRIES.map(industry => `  <url><loc>${BASE_URL}/products?industry=${industry.id}</loc><priority>0.8</priority></url>`),
+    '</urlset>'
+].join('\n');
+
+fs.writeFileSync('public/sitemap.xml', sitemap);
+console.log('Sitemap built.');
